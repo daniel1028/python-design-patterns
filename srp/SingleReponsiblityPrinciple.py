5657bbbbd23bb0693cef002d2f28bc58812df7b7
Daniel <arokia78@gmail.com>
2019-12-28 10:20:24 +0530

    Single Responsiblity Principle - Pattern

---
+++ srp/SingleReponsiblityPrinciple.py
@@ -0,0 +1,51 @@
+"""
+Design pattern called, Single Reponsiblity Principle
+In each class we should have relevant functions.
+
+"""
+
+
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.count += 1
+        self.entries.append(f'{self.count}: {text}')
+
+    def remove_entry(self, position):
+        del self.entries[position]
+
+    def __str__(self):
+        return '\n'.join(self.entries)
+
+
+# load and save functions are irrelavnat to this class. It should be moved to new class
+# def save(self, filename):
+#     file = open(filename, 'w')
+#     file.write(str(self))
+#     file.close()
+#
+# def load(self):
+#     pass
+
+class PersistenceManager:
+    @staticmethod
+    def save_to_file(journal, file):
+        file = open(file, 'w')
+        file.write(str(journal))
+        file.close()
+
+
+j1 = Journal()
+j1.add_entry("I feel very bad today")
+j1.add_entry("I cried today")
+j1.add_entry("All is well")
+#print(j1)
+
+file = r'./journals.txt'
+PersistenceManager.save_to_file(j1,file)
+
+with open(file,'r') as rh:
+    print(rh.read())
